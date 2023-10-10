54c2b8e822953900500d44e4bac930de2180d4f8
PagezLwando <lwandonodume@gmail.com>
2023-10-11 01:29:27 +0200

    Calculated age method

--- src/main/java/com/enviro/assessment/grad001/lwandonodume/backend/services/Impl/ProductServiceImpl.java
+++ src/main/java/com/enviro/assessment/grad001/lwandonodume/backend/services/Impl/ProductServiceImpl.java
@@ -1,21 +1,38 @@
 package com.enviro.assessment.grad001.lwandonodume.backend.services.Impl;
 
+import com.enviro.assessment.grad001.lwandonodume.backend.exceptions.AgeException;
+import com.enviro.assessment.grad001.lwandonodume.backend.models.Investor;
 import com.enviro.assessment.grad001.lwandonodume.backend.models.Product;
+import com.enviro.assessment.grad001.lwandonodume.backend.models.Type;
 import com.enviro.assessment.grad001.lwandonodume.backend.repository.ProductRepository;
+import com.enviro.assessment.grad001.lwandonodume.backend.services.InvestorService;
 import com.enviro.assessment.grad001.lwandonodume.backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Autowired
+    InvestorService investorService;
 
     @Autowired
     ProductRepository productRepository;
 
     @Override
-    public Product save(Product product) {
+    public Product save(Product product){
+        Investor investor = investorService.findById(product.getInvestorId());
+        int age = calculateAge(investor.getDateOfBirth());
+        try {
+            if (product.getType().equals(Type.RETIREMENT) && age >= 65) {
+                throw new AgeException("Age must be less than or equal to 65.");
+            }
+        } catch (Exception e) {
+            System.err.println("Unexpected exception: " + e.getMessage());
+        }
         return productRepository.save(product);
     }
 
@@ -27,5 +44,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> findAllProducts() {
         return productRepository.findAll();
+    }
+
+    private int calculateAge(LocalDate dateOfBirth) {
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(dateOfBirth, now);
+        return period.getYears();
     }
 }
