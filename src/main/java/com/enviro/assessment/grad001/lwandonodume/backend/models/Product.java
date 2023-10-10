8e2b22dd274df6f87d0107f54c19764344463221
PagezLwando <lwandonodume@gmail.com>
2023-10-10 22:59:19 +0200

    Added getters and setters

--- src/main/java/com/enviro/assessment/grad001/lwandonodume/backend/models/Product.java
+++ src/main/java/com/enviro/assessment/grad001/lwandonodume/backend/models/Product.java
@@ -1,4 +1,30 @@
 package com.enviro.assessment.grad001.lwandonodume.backend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Product")
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private Type type;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "current_balance")
+    private Long currentBalance;
+    @Column(name = "latest_balance")
+    private Long latestBalance;
 }
