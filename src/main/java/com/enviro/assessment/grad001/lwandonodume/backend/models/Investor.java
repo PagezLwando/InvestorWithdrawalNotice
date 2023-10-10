75cabbbc560e90ae84c7e50bd15b087189c0f13b
PagezLwando <lwandonodume@gmail.com>
2023-10-10 21:17:58 +0200

    Added Investor model

---
+++ src/main/java/com/enviro/assessment/grad001/lwandonodume/backend/models/Investor.java
@@ -0,0 +1,31 @@
+package com.enviro.assessment.grad001.lwandonodume.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Investor")
+public class Investor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_address")
+    private String emailAddress;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "address")
+    private String address;
+
+}
